e2110c0050ba4044be625d281de9afecc5901a2b
Александр Елисеев <a.eliseev@demiusgroup.ru>
2025-04-03 20:59:18 +0300

    change useEffect

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { MovieItemProps } from '../types/movie';
 import { Typography } from 'antd';
@@ -9,7 +9,7 @@ export const MoviePage = () => {
   const location = useLocation();
   const [movieDetails, setMovieDetails] = useState<MovieItemProps | undefined>();
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (location.state) {
       setMovieDetails(location.state);
     }
